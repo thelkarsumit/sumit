de8c5fd36a36989279d016376669ecba1ec1dcad
Sumit <sumitthelkar30799@gmail.com>
2024-01-04 23:19:05 +0530

    Again reverted by Right Value

--- mynewfile.py
+++ mynewfile.py
@@ -4,8 +4,5 @@ print("Sumit is Cloud Engineer 1")
 print("Sumit is Cloud Engineer 2")
 print("Sumit is Cloud Engineer 3")
 print("Sumit is Cloud Engineer 4")
-<<<<<<< HEAD
-=======
 print("Sumit is Cloud Engineer 5")
 print("Sumit is Cloud Engineer 6")
->>>>>>> parent of 352fefa (Adding new print line for CE7)
